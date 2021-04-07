ea496ea6206aae5bbaade01212a51f49f32204e5
Michael Chen <chenmich@flip1.engr.oregonstate.edu>
2021-04-07 00:46:09 -0700

    Determines if a year is a leap year

--- 1hw/michael_chen_hw1.py
+++ 1hw/michael_chen_hw1.py
@@ -1,7 +1,19 @@
-def main():
-	number = 2000
+import sys
+import math
 
+#get the number from the command line
+number = int(sys.argv[1])
 
+#conditional for results
+leap = " not"
+	
+#check if the year is a leap year
+if number % 4 == 0:
+	if number % 400.0 == 0:
+		leap = ""
+	else:
+		if not (number % 100.0 == 0):
+			leap = ""
 
-
-main()
+#print result
+print(f"{number} is{leap} a leap year")
