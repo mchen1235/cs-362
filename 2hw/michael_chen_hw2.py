d4c5d49ed36bc8ebbac5fb2b1192eb4c5a33ed22
mchen1235 <michaelschen123@gmail.com>
2021-04-21 00:47:43 -0700

    Moved code to a function

--- 2hw/michael_chen_hw2.py
+++ 2hw/michael_chen_hw2.py
@@ -1,19 +1,23 @@
 import sys
 import math
 
-#get the number from the command line
-number = int(sys.argv[1])
+def main():
+	#get the number from the command line
+	number = int(sys.argv[1])
 
-#conditional for results
-leap = " not"
-	
-#check if the year is a leap year
-if number % 4 == 0:
-	if number % 400.0 == 0:
-		leap = ""
-	else:
-		if not (number % 100.0 == 0):
+	#conditional for results
+	leap = " not"
+		
+	#check if the year is a leap year
+	if number % 4 == 0:
+		if number % 400.0 == 0:
 			leap = ""
+		else:
+			if not (number % 100.0 == 0):
+				leap = ""
 
-#print result
-print(f"{number} is{leap} a leap year")
+	#print result
+	print(f"{number} is{leap} a leap year")
+	return 0;
+
+main()
