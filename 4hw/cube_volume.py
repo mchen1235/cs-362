8ccf534eb9fb20ecf37f239c22725b38ded1c6ce
mchen1235 <michaelschen123@gmail.com>
2021-04-27 00:37:52 -0700

    returns volume

--- 4hw/cube_volume.py
+++ 4hw/cube_volume.py
@@ -1,7 +1,10 @@
 def cube():
     edge_length = int(input("What is the length of the edge of your cube: "))
+    
     volume = edge_length * edge_length * edge_length
     print(f"The volume of your cube is {volume}")
+
+    return volume
 
 
 cube()
