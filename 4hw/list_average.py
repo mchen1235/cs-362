881e9d04cb4e6fda4c557fe183c66cdb5d74fcab
mchen1235 <michaelschen123@gmail.com>
2021-04-27 17:43:13 -0700

    Moved input to parameter

--- 4hw/list_average.py
+++ 4hw/list_average.py
@@ -1,20 +1,10 @@
-def list_average():
-    new_list = []
+def list_average(new_list):
     total = 0
     average = 0
-    elements = int(input("How many numbers do you want in the list: "))
-
-    #create list
-    for i in range(elements):
-        element = int(input("Enter a number to add: "))
-        new_list.append(element)
 
     #calculate average
     for i in new_list:
         total += i
     average = total / elements
-    print(f"The average is {average}")
 
     return average
-
-list_average()
