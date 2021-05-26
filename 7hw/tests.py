ea693e0aae9294335236d31a2265e34f416eca1f
mchen1235 <michaelschen123@gmail.com>
2021-05-25 22:46:52 -0700

    Wrote 4th test that checks if every number that is divisible by 3 and 5 is replaced with FizzBuzz

--- 7hw/tests.py
+++ 7hw/tests.py
@@ -22,5 +22,8 @@ class testCaseFizzbuzz(unittest.TestCase):
     def test3(self):
         self.assertEqual(fb.fizzbuzz(6), "1 2 Fizz 4 Buzz Fizz ")
 
+    def test4(self):
+        self.assertEqual(fb.fizzbuzz(16), "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 ")
+
 if __name__ == '__main__':
     unittest.main()
